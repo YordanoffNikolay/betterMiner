3245da9f271be4bb1a187fd81259b8b9e26457ca
Nik0 <yordanoff.nikolay@gmail.com>
2024-04-19 23:15:36 +0300

    reworked landing page

--- src/main/java/org/yordanoffnikolay/betterminer/controllers/mvc/MvcController.java
+++ src/main/java/org/yordanoffnikolay/betterminer/controllers/mvc/MvcController.java
@@ -3,11 +3,15 @@ package org.yordanoffnikolay.betterminer.controllers.mvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.yordanoffnikolay.betterminer.dtos.DateRange;
 import org.yordanoffnikolay.betterminer.services.ArticleService;
+
+import java.text.ParseException;
 
 @Controller
 @RequestMapping()
@@ -28,9 +32,18 @@ public class MvcController {
     }
 
     @PostMapping
-    public String getArticlesByDateRange(DateRange dateRange, Model model) {
-        model.addAttribute("articles", articleService.getArticlesByDateRange(dateRange));
-        model.addAttribute("dateRange", dateRange);
+    public String getArticlesByDateRange(@ModelAttribute("dateRange") DateRange dateRange, Model model,
+                                         BindingResult bindingResult) throws ParseException {
+        if (bindingResult.hasErrors()) {
+            return "index";
+        }
+
+        try {
+            model.addAttribute("articles", articleService.getArticlesByDateRange(dateRange));
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "index";
+        }
         return "results";
     }
 }
